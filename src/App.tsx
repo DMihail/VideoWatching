f10f09025f385b6cb899a9bf1849bed5543eaf46
DMihail <dzezelomihail@gmail.com>
2024-04-20 15:35:35 +0300

    remove debug button

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import {StatusBar, Button} from 'react-native';
+import {StatusBar} from 'react-native';
 import {
   initialWindowMetrics,
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
-// @ts-ignore
-import NativeDevSettings from 'react-native/Libraries/NativeModules/specs/NativeDevSettings';
 import {persist, store} from './redux';
 import Navigation from './navigation';
 import {COLORS} from './rules/COLORS.ts';
 
 function App(): React.JSX.Element {
-  const connectToRemoteDebugger = () => {
-    NativeDevSettings.setIsDebuggingRemotely(true);
-  };
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persist}>
         <SafeAreaProvider initialMetrics={initialWindowMetrics}>
           <StatusBar backgroundColor={COLORS.black} />
-          {/*{__DEV__ && (*/}
-          {/*  <Button title={'Debug'} onPress={connectToRemoteDebugger} />*/}
-          {/*)}*/}
           <Navigation />
         </SafeAreaProvider>
       </PersistGate>
